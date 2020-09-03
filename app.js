4d858fc4e7eec6ab427c011bb0fc42364fc8c8c1
gtvilela <gabrielathalitasv@gmail.com>
2020-09-03 18:23:12 -0300

    Encapsulando variáveis de ambiente com dotenv

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import { studentRouter } from './routes/studentRouter.js';
+require('dotenv').config();
 
 //Conexão MongoDB
 conexaoMongo();
+const { USERDB, PWDDB, PORT } = process.env;
 
 async function conexaoMongo() {
   try {
     await mongoose.connect(
-      'mongodb+srv://admin:gt651200@bootcampfullstack.5rvru.mongodb.net/bootcamp?retryWrites=true&w=majority',
+      `mongodb+srv://${USERDB}:${PWDDB}0@bootcampfullstack.5rvru.mongodb.net/bootcamp?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -26,4 +28,4 @@ const app = express();
 app.use(express.json());
 app.use(studentRouter);
 
-app.listen(3000, () => console.log('API Iniciada'));
+app.listen(PORT, () => console.log('API Iniciada'));
